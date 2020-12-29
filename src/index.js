85224c0293e67b8006f97bb8378f524b9001d4a0
Victorsloop <torres.victor136@gmail.com>
2020-12-28 20:41:58 -0500

    Thunk

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
-import {createStore} from 'redux'
+import {createStore,applyMiddleware} from 'redux'
 import rootReducer from './Redux/rootReducer'
 import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 store.dispatch({type: "INCREMENT_COUNTER"})
 
 ReactDOM.render(
